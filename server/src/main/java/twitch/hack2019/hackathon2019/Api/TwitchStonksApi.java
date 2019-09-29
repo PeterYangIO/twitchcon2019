bc3393b2f265a12fc546f248df3394d4d3a8d4fb
Jonathan Chai <chaijw@usc.edu>
2019-09-28 22:52:14 -0700

    fix stuff

--- server/src/main/java/twitch/hack2019/hackathon2019/Api/TwitchStonksApi.java
+++ server/src/main/java/twitch/hack2019/hackathon2019/Api/TwitchStonksApi.java
@@ -1,22 +1,27 @@
 package twitch.hack2019.hackathon2019.Api;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import twitch.hack2019.hackathon2019.models.StreamerDto;
 import twitch.hack2019.hackathon2019.repo.StreamerRepo;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 public class TwitchStonksApi {
 
+    @Autowired
     private StreamerRepo sRepo;
 
+    @RequestMapping("/")
+    public String checkconn()
+    {
+        return "Connected";
+    }
 
     @PostMapping("/addstreamer")
-    public StreamerDto addStreamer(@RequestBody String body)
+    public StreamerDto addStreamer(@RequestParam String body)
     {
         return sRepo.save(new StreamerDto(body));
     }
--- server/src/main/java/twitch/hack2019/hackathon2019/repo/StreamerRepo.java
+++ server/src/main/java/twitch/hack2019/hackathon2019/repo/StreamerRepo.java
@@ -1,9 +1,11 @@
 package twitch.hack2019.hackathon2019.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import twitch.hack2019.hackathon2019.models.StreamerDto;
 
-public interface StreamerRepo extends CrudRepository<StreamerDto, Integer> {
+
+public interface StreamerRepo extends JpaRepository<StreamerDto, Integer> {
 
     
 
